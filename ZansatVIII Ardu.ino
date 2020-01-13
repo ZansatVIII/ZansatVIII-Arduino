01692ae75e40da475e8f112b89dcc86102e9e06c
ZansatVIII <58602932+ZansatVIII@users.noreply.github.com>
2020-01-13 18:56:31 +0100

    SD-card Logging, Comment Augmentation, servo init

--- ZansatVIII Ardu.ino
+++ ZansatVIII Ardu.ino
@@ -1,3 +1,4 @@
+///////////////////////////////////////////////////////////Libs\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <SPI.h>
@@ -5,31 +6,39 @@
 #include <Adafruit_BME280.h>
 #include <TinyGPS.h>  
 #include <Adafruit_HMC5883_U.h>
-
+#include <SD.h>
+#include <Servo.h>
+///////////////////////////////////////////////////////Defines and vars\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 //char Buffer[40]; 
 String out; 
 float initp = 1013.25 , declination = 1.0 * degtorad /*default*/,drive, heading, course;
 const float degtorad = 71 / 4068 ,  ; //initial pressure and declination, needs adjusting every time starting
 long lon , lat , Tlon, Tlat; //longtitude , lattitude , Target longtitude , Target lattitude
 unsigned long fix_age;
-bool servoEN = False; //Servo motors responsible for guidance are disabled at the start and can be enabled by command
+bool servoEN = False;//Servo motors responsible for guidance are disabled at the start and can be enabled by command
 
 #define GPSIN  8
 #define GPSOUT 7
 #define GPSPPS 3
+#define SDSS 13
+#define SRVL 5
+#define SRVR 6
 
 SoftwareSerial GPS(GPSIN,GPSOUT); //GPS 
 TinyGPS gps;
 Adafruit_BME280 bme; // I2C
 Adafruit_HMC5883_Unified mag;
-
-//Sends information every interrupt
+Servo SL,SR
+///////////////////////////////////////////////////Sends all data every interrupt/////////////////////////////
 void Tick(){
    out = out + String(bme.readTemperature()); 
    out = out + String(bme.readPressure()); 
    out = out + String(bme.readAltitude(initp)); 
    out = out + String(bme.readHumidity());
+   out = out + String(drive());
+   out = out + String()
    Serial.println(out);
+   if(log) log.println(out)
    out = ''
   //Formats all data onto a buffer that is printed in string form in the next line
   //sprintf(outBuffer,"%s ; %s ; %s ; %s ; %s ",String(bme.readTemperature()),String(bme.readPressure()),String(bme.readAltitude(1013.25)),String(bme.readHumidity()),String(drive));
@@ -37,25 +46,42 @@ void Tick(){
 	
 	
 }
-
+////////////////////////////////////////////Hardware and Library initialization\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 void setup() {
   GPS.begin(9600);
   Serial.begin(9600);
   pinMode(2,INPUT);
-	//Attaches interrupt to the pin recieving the pps signal
-  attachInterrupt(digitalPinToInterrupt(GPSPPS), Tick , RISING);
+  //Initializes all logging and measurement capabilites
+  if(SD.begin(SDSS)){
+    File log = SD.open(log.txt, FILE_WRITE);
+    if(log){
+      out = 'ST' 
+    }
+    else {
+      out = F!('SO')
+    }
+  }
+  else{
+    bool log = False 
+    out = F("S");
+  }
   if (!bme.begin()) {  
-    Serial.println(F("NO BME"));
+    out = out + F("B");
     while(1);
+  else{
+    initp = bme.readPressure();
+  }
   }
   if(!mag.begin()){
-    Serial.println(F("NO HMC"));
+    out = out + F("M");
     while(1);
   }
-
+  SL.attach(SRVL);
+  SR.attach(SRVR);
 }
 
 void loop() {
+/////////////////////////////BEGIN OF LOOP | Orientation data gathering \\\\\\\\\\\\\\\\\\\\\\\
   //Gets Longtitude And Lattitude from the gps
   while (GPS.available())
   {
@@ -65,7 +91,7 @@ void loop() {
 			
     }
   }
-  //Makes a sensor event and reads all sensor data to it
+  //Makes a sensor event and reads all magnetometer data to it
   sensors_event_t event;
   mag.getEvent(&event); 
   //Reads calculates the direction we are heading. TODO: Check Sensor orientation and change formula accordingly 
@@ -76,15 +102,17 @@ void loop() {
   else if(heading > 360){ // Check for wrap due to addition of declination.
     heading -= 360;
   }
-  //Calculate Heading to target  N = 0 E = 90 S = 180 W = 270
+//////////////////////////////////////Drive Calculation\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\  
+  //Calculate Course to target  N = 0 E = 90 S = 180 W = 270
   course = course_to(lat,lon,Tlat,Tlon);
   /*Calculating the drive variable in two steps: 
   Step one is to calculate the change in angle needed to make heading equal to the course.
   Step two is to calculate the (propotional) drive by dividing the step one number with the change of angle in degrees that we want the drive to be maximum and clamp the result between the maximal and the minimal drive value
+  0 is left 90 is right
   */
-  drive = (course - heading) / 45
+  drive = (course - heading +) / 45
   drive = constrain(drive, -1 ,1);
-  //Recieving commands from the ground
+ ////////////////////////////////Recieving commands from the ground\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 	if(Serial.available() > 1){
     sIN = Serial.readString();
 		switch(sIN.charAt(0)){ //Commands: C ARG1 ARG2 ARG3 Example: s 1 
