7b122c630a19f0bcd71ce01ebfa1951eaed3d199
ZansatVIII <58602932+ZansatVIII@users.noreply.github.com>
2020-01-24 20:10:02 +0100

    Create Chiptemp.cpp

---
+++ Chiptemp/Chiptemp.cpp
@@ -0,0 +1,30 @@
+
+#include "Chiptemp.h"
+
+//INIT 
+void Chiptemp:Chiptemp(int s){
+  
+  Chiptemp::samplesize = s
+ 
+  //Copied Code
+  ADCSRA |= _BV(ADSC); // start the conversion
+  while (bit_is_set(ADCSRA, ADSC)); // ADSC is cleared when the conversion finishes
+  return (ADCL | (ADCH << 8)) - 342; // combine bytes & correct for temp offset (approximate)}
+  
+}
+//read temperature once 
+int Chiptemp::Sample()
+{
+ //Copied code
+ ADCSRA |= _BV(ADSC); // start the conversion
+ while (bit_is_set(ADCSRA, ADSC)); // ADSC is cleared when the conversion finishes
+ return (ADCL | (ADCH << 8)) - 342; // combine bytes & correct for temp offset (approximate)}
+}
+
+//Average the temperatures 
+float Chiptemp::GetRead(){
+  //one reading discarded, safety feature 
+  Chiptemp::Sample();
+  for (int i = 1; i < 1000; i++) // start at 1 so we dont divide by 0
+    current += ((Chiptemp::Sample() - current)/(float)i);
+}
