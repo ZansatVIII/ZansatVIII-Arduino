21646b3f7333213709e0b1bf989e6852c05555ae
ZansatVIII <58602932+ZansatVIII@users.noreply.github.com>
2020-01-29 20:33:37 +0100

    Create NOGPS Branch
    Now the timing is millis based

--- ZansatVIIIArduino.ino
+++ ZansatVIIIArduino.ino
@@ -23,24 +23,25 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
-#include <TinyGPS.h>  
+//#include <TinyGPS.h>  
 #include <QMC5883LCompass.h>
 #include <SD.h>
 #include <Servo.h>
 /*///////////////////////////////////////////////////////Defines and vars\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
 
 String out; 
-static float pack [10] = {0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 1000.0 , 0.0, 0.0 , 2.0};
-static long lon , lat , Tlon, Tlat; //longtitude , lattitude , Target longtitude , Target lattitude
-static unsigned long fix_age;
+static float pack [10] = {0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 1000.0, 0.0 , 2.0};
+static long prevtick;
+//static long lon , lat , Tlon, Tlat; //longtitude , lattitude , Target longtitude , Target lattitude
+//static unsigned long fix_age;   GPS
 static bool servoEN = false; //Servo motors responsible for guidance are disabled at the start and can be enabled by command
 static File Log ;
 static Servo SL , SR;
 /*/////////////PIN MAPPING\\\\\\\\\\\\\\\\\*/
 
-#define GPSIN  8
-#define GPSOUT 7
-#define GPSPPS 3
+//#define GPSIN  8  //NOGPS 
+//#define GPSOUT 7
+//#define GPSPPS 3
 #define SDSS 13
 #define SRVL 5
 #define SRVR 6
@@ -62,8 +63,8 @@ static Servo SL , SR;
 /*////////////EEPROM MAPPING\\\\\\\\\\\\\\\\
 //(EEPROM usage is considered and not implemented yet) */
 
-SoftwareSerial GPS(GPSIN,GPSOUT); //GPS 
-TinyGPS gps;
+//SoftwareSerial GPS(GPSIN,GPSOUT); //NOGPS 
+//TinyGPS gps;
 Adafruit_BME280 bme; // I2C
 QMC5883LCompass mag;
 
@@ -79,6 +80,8 @@ void INIT(){
     else {
       out = 'O';
     }
+    pinMode(SRVL,OUTPUT);
+    pinMode(SRVL,OUTPUT);
     SL.attach(SRVL);
     SR.attach(SRVR);
   }
@@ -90,8 +93,8 @@ void Tick(){
    pack [PRESS] = bme.readPressure(); 
    pack [HEIGHT] = bme.readAltitude(pack[INITP]); 
    pack [HUMIDITY] = bme.readHumidity();
-   out = out + String(lon, HEX);
-   out = out + String(lat, HEX);
+   //out = out + String(lon, HEX);  //NOGPS 
+   //out = out + String(lat, HEX);
    if (servoEN) tone(2,1500,500);
    for(int i = 0; i <= 4; i ++){
      Serial.print(pack[i], 5);
@@ -109,7 +112,7 @@ void Tick(){
 /*///////////////////////////////////////////Hardware and Library initialization\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
 
 void setup() {
-  GPS.begin(9600);
+  //GPS.begin(9600); NOGPS 
   Serial.begin(9600);
   pinMode(3,INPUT);
   pinMode(2,OUTPUT);
@@ -124,7 +127,7 @@ void setup() {
   mag.init();
   
   //Attaches interrupt to the pin recieving the pps signal
-  attachInterrupt(digitalPinToInterrupt(GPSPPS), Tick , RISING);
+  //attachInterrupt(digitalPinToInterrupt(GPSPPS), Tick , RISING); //NOGPS
 }
 
 void loop() {
@@ -140,7 +143,7 @@ void loop() {
   
   
   //Gets Longtitude And Lattitude from the gps
-  while (GPS.available())
+  /*while (GPS.available())   //NOGPS 
   {
     if (gps.encode(GPS.read()))
     {
@@ -148,19 +151,20 @@ void loop() {
       
     }
   }
+  */
   //Reads calculates the direction we are heading (in degrees). TODO: Check Sensor orientation and change formula accordingly 
   mag.read();
   pack[HEADING] = mag.getAzimuth() + pack[DECL];
   //pack[HEADING] = (atan2(mag.getY(), mag.getX())/ 57,29577) + pack[DECL]; //The old way using the raw data, may need to be recosidered depending on sensor orientation
   if(pack[HEADING] < 0){ //Check if the value is negative
-    pack[HEADING] += 360;
+    pack[HEADING] += 360.0;
   } 
   else if(pack[HEADING] > 360){ // Check for wrap due to addition of declination.
-    pack[HEADING] -= 360;
+    pack[HEADING] -= 360.0;
   }
 //////////////////////////////////////Drive Calculation\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\  
   //Calculate Course (pack[5]) to target  N = 0 E = 90 S = 180 W = 270 , Own heading must obey the same rules
-  pack[COURSE] = TinyGPS::course_to(lat,lon,Tlat,Tlon);
+  //pack[COURSE] = TinyGPS::course_to(lat,lon,Tlat,Tlon); //NOGPS
   /*Calculating the drive variable in two steps: 
   Step one is to calculate the change in angle needed to make heading equal to the course.
   Step two is to calculate the (propotional) drive by dividing the step one number with the change of angle in degrees that we want the drive to be maximum and clamp the result between the maximal and the minimal drive value
@@ -172,17 +176,21 @@ void loop() {
   SL.write( -(constrain(pack[DRIVE], -180 , 0)));
   //Rotates when SL at 0 an doesnt turn when it is on 0 itself; full whinch at +180  
   SR.write(constrain(pack[DRIVE], 0, 180));
- 
+  if(millis() - prevtick >= 1000){
+    Tick();
+    prevtick = millis();
+  } 
   /*///////////////////////////////Recieving commands from the ground\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
   
   if(Serial.available() > 1){
     String sIN = Serial.readString();
   switch(sIN[0]){ //Commands: C ARG1,ARG2;ARG3 Example: s 1 
-    case char('v'): //Setting Victim Coordinates 
+    /*case char('v'): //Setting Victim Coordinates NOGPS
       int spac = sIN.indexOf(',');
       Tlat = sIN.substring(2, spac).toInt();
       Tlon = sIN.substring(spac).toInt();
       break;
+    */
     case char('p'): //Setting initial pressure and declination
       pack[sIN[3]] = sIN.substring(4).toFloat();
       break;
