333248135c2cd35433c604f5f49bf0ca3bd0c0a8
ZansatVIII <58602932+ZansatVIII@users.noreply.github.com>
2020-02-04 20:11:23 +0100

    Update servotest.ino

--- servotest.ino
+++ servotest.ino
@@ -22,16 +22,17 @@ void loop() {
   target = wrapcheck(target + 1); 
   drive = wrapcheck(target - rotate);
   drive = (drive - 180.0F) * 2.0F;
-  Serial.println(drive);
+  Serial.print(drive);
   drive = constrain(drive, -180.0F , 180.0F);
-  Serial.println(drive);
+  Serial.print(drive);
   //-180 - Fully Winched In, turning ; 0 - Fully Winched Out, no turning.
   float calc = constrain(drive, -180.0F , 0.0F);
-  Serial.println(calc);
+  Serial.print(calc);
   SL.write(calc * -1.0F );
   //Rotates when SL at 0 an doesnt turn when it is on 0 itself; full whinch at +180
   calc =  constrain(drive, 0.0F, 180.0F);
-  Serial.println(calc);
+  Serial.print(calc);
   SR.write(calc);
+  Serial.print("\n")
 
 }
